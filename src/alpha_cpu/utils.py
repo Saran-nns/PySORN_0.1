78835a68314e1dbda4c8fc454995791fb61e86f5
Saranraj Nambusubramaniyan <sarannsworld@gmail.com>
2019-02-15 17:49:32 +0100

    Simulation analysis plot and statistics class methods
    Update Training plots and statistics methods

--- src/alpha_cpu/utils.py
+++ src/alpha_cpu/utils.py
@@ -8,7 +8,13 @@ import numpy as np
 from scipy.stats import norm
 import random
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.optimize import curve_fit
+from scipy.stats import norm
+from scipy import stats
 
+from matplotlib import pylab
+from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
 
 # Random seeds
 
@@ -369,13 +375,14 @@ class Plotter(object):
         pass
 
     @staticmethod
-    def hist_incoming_conn(weights, bin_size, histtype, gaussian_fit):
+    def hist_incoming_conn(weights, bin_size, histtype, gaussian_fit, savefig):
 
         """Args:
         :param weights(array) - Connection weights
         :param bin_size(int) - Histogram bin size
         :param histtype(str) - Same as histtype matplotlib
         :param gaussian_fit(bool) - If true; returns the plot with gaussian fit for corresponding histogram
+        :param savefig(bool) - If True plot will be saved as png file in the cwd
 
         Returns:
         plot object """
@@ -404,16 +411,20 @@ class Plotter(object):
             plt.axvline(x=avg, color='r', linestyle='--', label='Mean')
             plt.legend()
 
+        if savefig:
+            plt.savefig('hist_incoming_conn')
+
         return plt.show()
 
     @staticmethod
-    def hist_outgoing_conn(weights, bin_size, histtype, gaussian_fit):
+    def hist_outgoing_conn(weights, bin_size, histtype, gaussian_fit, savefig):
 
         """Args:
                 :param weights(array) - Connection weights
                 :param bin_size(int) - Histogram bin size
                 :param histtype(str) - Same as histtype matplotlib
-                :param gaussian_fit(bool) - If true; returns the plot with gaussian fit for corresponding histogram
+                :param gaussian_fit(bool) - If True; returns the plot with gaussian fit for corresponding histogram
+                :param savefig(bool) - If True plot will be saved as png file in the cwd
 
                 Returns:
                 plot object """
@@ -442,5 +453,495 @@ class Plotter(object):
             plt.axvline(x=avg, color='r', linestyle='--', label='Mean')
             plt.legend()
 
+        if savefig:
+            plt.savefig('hist_outgoing_conn')
+
         return plt.show()
 
+    @staticmethod
+    def network_connection_dynamics(connection_counts, savefig):
+
+        """Args:
+        :param connection_counts(array) - 1D Array of number of connections in the network per time step
+        :param savefig(bool) - If True plot will be saved as png file in the cwd
+        Returns:
+        plot object"""
+
+        # Plot graph for entire simulation time period
+        fig1, ax1 = plt.subplots(figsize=(12, 5))
+        ax1.plot(connection_counts, label='Connection dynamics')
+        plt.margins(x=0)
+        ax1.set_xticks(ax1.get_xticks()[::2])
+
+        ax1.set_title("Network connection dynamics")
+        plt.ylabel('Number of active connections')
+        plt.xlabel('Time step')
+        plt.legend(loc='upper right')
+        plt.tight_layout()
+
+        # Inset plot for initial simulation steps
+
+        ax2 = plt.axes([0, 0, 1, 1])
+
+        # Set the position and relative size of the inset axes within ax1
+        ip = InsetPosition(ax1, [0.25, 0.4, 0.3, 0.3])
+        ax2.set_axes_locator(ip)
+        ax2.plot(connection_counts[0:10000])
+        plt.margins(x=0)
+        ax2.set_title('Initial 10000 time steps of Decay Phase')
+        ax2.set_xticks(ax2.get_xticks()[::2])
+
+        # End Inset plot
+        ax3 = plt.axes([0, 0, 0, 0])
+
+        # Set the position and relative size of the inset axes within ax1
+        ip1 = InsetPosition(ax1, [0.6, 0.4, 0.3, 0.3])
+        ax3.set_axes_locator(ip1)
+        # Plot the last 10000 time steps
+        ax3.plot(connection_counts[-10000:])
+        plt.margins(x=0)
+        ax3.set_title('Final 10000 time steps of Stable Phase')
+        ax3.set_xticks(ax3.get_xticks()[::1])
+
+        # Uncomment to show decay and stable phase in colors
+        # ax1.axvspan(0, 200000, alpha=0.1, color='red')
+        # ax2.axvspan(0, 10000, alpha=0.1, color='red')
+        # ax1.axvspan(200000, 1000000, alpha=0.1, color='green')
+
+        if savefig:
+            plt.savefig('connection_dynamics')
+
+        return plt.show()
+
+    @staticmethod
+    def hist_firing_rate_network(spike_train, bin_size, savefig):
+
+        """Args:
+        :param spike_train(array) - Array of spike trains
+        :param bin_size(int) - Histogram bin size
+        :param savefig(bool) - If True, plot will be saved in the cwd
+
+        Returns: plot object """
+
+        fr = np.count_nonzero(spike_train.tolist(), 1)
+
+        # Filter zero entries in firing rate list above
+        fr = list(filter(lambda a: a != 0, fr))
+        plt.title('Distribution of population activity without inactive time steps')
+        plt.xlabel('Spikes/time step')
+        plt.ylabel('Count')
+
+        plt.hist(fr, bin_size)
+
+        if savefig:
+            plt.savefig('hist_firing_rate_network.png')
+
+        return plt.show()
+
+    @staticmethod
+    def scatter_plot(spike_train, with_firing_rates, savefig):
+
+        """Args:
+            :param spike_train (array) - Array of spike trains
+            :param with_firing_rates(bool) - If True, firing rate of the network will be plotted
+            :param savefig(bool) - If True, plot will be saved in the cwd
+
+           Returns:
+            plot object"""
+
+        # Get the indices where spike_train is 1
+        x, y = np.argwhere(spike_train.T == 1).T
+
+        plt.figure(figsize=(8, 5))
+
+        if with_firing_rates:
+
+            firing_rates = Statistics.firing_rate_network(spike_train).tolist()
+            plt.plot(firing_rates, label='Firing rate')
+            plt.legend(loc='upper left')
+
+        plt.scatter(y, x, s=0.1, color='black')
+        # plt.plot(y,x,'|b')
+        # plt.gca().invert_yaxis()
+
+        plt.xlabel('Time(ms)')
+        plt.ylabel('Neuron #')
+        plt.legend(loc='upper left')
+
+        if savefig:
+            plt.savefig('ScatterSpikeTrain.png')
+        return plt.show()
+
+    @staticmethod
+    def raster_plot(spike_train, with_firing_rates, savefig):
+
+        """Args:
+                :param spike_train (array) - Array of spike trains
+                :param with_firing_rates(bool) - If True, firing rate of the network will be plotted
+                :param savefig(bool) - If True, plot will be saved in the cwd
+
+            Returns:
+                plot object"""
+
+        plt.figure(figsize=(11, 6))
+
+        if with_firing_rates:
+
+            firing_rates = Statistics.firing_rate_network(spike_train).tolist()
+            plt.plot(firing_rates, label='Firing rate')
+            plt.legend(loc='upper left')
+
+        # Get the indices where spike_train is 1
+        x, y = np.argwhere(spike_train.T == 1).T
+
+        plt.plot(y, x, '|r')
+
+        # plt.gca().invert_yaxis()
+        plt.xlabel('Time(ms)')
+        plt.ylabel('Neuron #')
+
+        if savefig:
+            plt.savefig('RasterSpikeTrain.png')
+        return plt.show()
+
+    @staticmethod
+    def correlation(corr, savefig):
+
+        """ Plot correlation between neurons"""
+
+        # Generate a mask for the upper triangle
+        mask = np.zeros_like(corr, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+
+        f, ax = plt.subplots(figsize=(11, 9))
+
+        # Custom diverging colormap
+        cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+        # Draw the heatmap with the mask and correct aspect ratio
+
+        sns.heatmap(corr, mask=mask, cmap=cmap, xticklabels=5, yticklabels=5, vmax=.1, center=0,
+                    square=False, linewidths=0.0, cbar_kws={"shrink": .9})
+        if savefig:
+            plt.savefig('Correlation between neurons')
+        return None
+
+    @staticmethod
+    def isi_exponential_fit(spike_train, neuron, bin_size, savefig):
+
+        """Args:
+            :param spike_train (array) - Array of spike trains
+            :param neuron(int) - If True, firing rate of the network will be plotted
+            :param bin_size(int) - Spike train will be splitted into bins of size bin_size
+            :param savefig(bool) - If True, plot will be saved in the cwd
+
+            Returns:
+            plot object"""
+
+        spike_time = Statistics.    spike_times(spike_train.T[neuron])  # Locate the spike time of the target neuron
+
+        isi = Statistics.spike_time_intervals(spike_time)  # ISI intervals of neuron
+
+        y, x = np.histogram(sorted(isi), bins=bin_size)
+
+        x = [int(i) for i in x]
+        y = [float(i) for i in y]
+
+        def exponential_func(y, a, b, c):
+            return a * np.exp(-b * np.array(y)) - c
+
+        # Curve fit
+        popt, pcov = curve_fit(exponential_func, x[1:bin_size], y[1:bin_size])
+
+        # Plot
+        plt.plot(x[1:bin_size], exponential_func(x[1:bin_size], *popt), label='Exponential fit')
+        plt.scatter(x[1:bin_size], y[1:bin_size], s=2.0, color='black', label='ISI')
+        plt.xlabel('ISI(time step)')
+        plt.ylabel('Frequency')
+        plt.legend()
+
+        if savefig:
+            plt.savefig('isi_exponential_fit')
+        return plt.show()
+
+    @staticmethod
+    def weight_distribution(weights, bin_size, savefig):
+        """Args:
+            :param weights (array) - Connection weights
+            :param bin_size(int) - Spike train will be splited into bins of size bin_size
+            :param savefig(bool) - If True, plot will be saved in the cwd
+
+            Returns:
+            plot object"""
+
+        weights = weights[weights >= 0.01]  # Remove the weight values less than 0.01 # As reported in article SORN 2013
+        y, x = np.histogram(weights, bins=bin_size)  # Create histogram with bin_size
+
+        plt.scatter(x[:-1], y, s=2.0, c='black')
+        plt.xlabel('Weight')
+        plt.ylabel('Frequency')
+
+        if savefig:
+            plt.savefig('weight distribution')
+
+        return plt.show()
+
+    @staticmethod
+    def linear_lognormal_fit(weights, savefig):
+
+        """Args:
+            :param weights (array) - Connection weights
+            :param savefig(bool) - If True, plot will be saved in the cwd
+
+            Returns:
+            plot object"""
+
+        X = weights.copy()
+
+        M = float(np.mean(X))  # Geometric mean
+        s = float(np.std(X))  # Geometric standard deviation
+
+        # Lognormal distribution parameters
+
+        mu = float(np.mean(np.log(X)))  # Mean of log(X)
+        sigma = float(np.std(np.log(X)))  # Standard deviation of log(X)
+        shape = sigma  # Scipy's shape parameter
+        scale = np.exp(mu)  # Scipy's scale parameter
+        median = np.exp(mu)
+
+        mode = np.exp(mu - sigma ** 2)  # Note that mode depends on both M and s
+        mean = np.exp(mu + (sigma ** 2 / 2))  # Note that mean depends on both M and s
+        x = np.linspace(min(weights), max(weights), num=100)  # values for x-axis
+
+        pdf = stats.lognorm.pdf(x, shape, loc=0, scale=scale)  # probability distribution
+
+        plt.figure(figsize=(12, 4.5))
+
+        # Figure on linear scale
+        plt.subplot(121)
+        plt.plot(x, pdf)
+
+        plt.vlines(mode, 0, pdf.max(), linestyle=':', label='Mode')
+        plt.vlines(mean, 0, stats.lognorm.pdf(mean, shape, loc=0, scale=scale), linestyle='--', color='green',
+                   label='Mean')
+        plt.vlines(median, 0, stats.lognorm.pdf(median, shape, loc=0, scale=scale), color='blue', label='Median')
+        plt.ylim(ymin=0)
+        plt.xlabel('Weight')
+        plt.title('Linear scale')
+        plt.legend()
+
+        # Figure on logarithmic scale
+        plt.subplot(122)
+        plt.semilogx(x, pdf)
+
+        plt.vlines(mode, 0, pdf.max(), linestyle=':', label='Mode')
+        plt.vlines(mean, 0, stats.lognorm.pdf(mean, shape, loc=0, scale=scale), linestyle='--', color='green',
+                   label='Mean')
+        plt.vlines(median, 0, stats.lognorm.pdf(median, shape, loc=0, scale=scale), color='blue', label='Median')
+        plt.ylim(ymin=0)
+        plt.xlabel('Weight')
+        plt.title('Logarithmic scale')
+        plt.legend()
+
+        if savefig:
+            plt.savefig('LinearLognormalFit')
+
+        return plt.show()
+
+
+    @staticmethod
+    def hamming(hamming_distance, savefig):
+
+        plt.figure(figsize=(15, 6))
+        plt.title("Hamming distance between actual and perturbed states")
+        plt.xlabel("Time steps")
+        plt.ylabel("Hamming distance")
+        plt.plot(hamming_distance)
+
+        if savefig:
+            plt.savefig('HammingDistance')
+
+        return plt.show()
+
+
+class Statistics(object):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def firing_rate_neuron(spike_train, neuron, bin_size):
+
+        # Measure spike rate of given neuron during given time window
+
+        """Args:
+                :param spike_train(array) - Array of spike trains
+                :param neuron(int) - Target neuron in the reservoir
+                :param bin_size(int) - Divide the spike trains into bins of size bin_size
+
+                Returns: firing_rate """
+
+        time_period = len(spike_train[:, 0])
+
+        neuron_spike_train = spike_train[:, neuron]
+
+        # Split the list(neuron_spike_train) into sub lists of length time_step
+        samples_spike_train = [neuron_spike_train[i:i + bin_size] for i in
+                               range(0, len(neuron_spike_train), bin_size)]
+
+        spike_rate = 0.
+
+        for idx, spike_train in enumerate(samples_spike_train):
+            spike_rate += list(spike_train).count(1.)
+
+        spike_rate = spike_rate * bin_size / time_period
+
+        # print('Firing rate of neuron %s in %s time steps is %s' %(neuron,time_step,spike_rate/time_step))
+
+        return time_period, bin_size, spike_rate
+
+    @staticmethod
+    def firing_rate_network(spike_train):
+
+        """Args:
+        :param spike_train(array) - Array of spike trains
+
+        Returns: firing_rate """
+
+        firing_rate = np.count_nonzero(spike_train.tolist(), 1)
+
+        return firing_rate
+
+    @staticmethod
+    def scale_dependent_smoothness_measure(firing_rates):
+
+        # Smaller values corresponds to smoother series
+        """
+        Args:
+
+        firing_rates - List of number of active neurons per time step
+
+        Returns:
+
+        sd_diff - Float value signifies the smoothness of the semantic changes in firing rates
+        """
+
+        diff = np.diff(firing_rates)
+        sd_diff = np.std(diff)
+
+        return sd_diff
+
+    @staticmethod
+    def scale_independent_smoothness_measure(firing_rates):
+
+        # Smaller values corresponds to smoother series
+        """
+        Args:
+        firing_rates - List of number of active neurons per time step
+
+        Returns:
+        coeff_var - Float value signifies the smoothness of the semantic changes in firing rates """
+
+        diff = np.diff(firing_rates)
+        mean_diff = np.mean(diff)
+        sd_diff = np.std(diff)
+
+        coeff_var = sd_diff / abs(mean_diff)
+
+        return coeff_var
+
+    # Using one-lag auto-correlation measure
+
+    @staticmethod
+    def autocorr(firing_rates, t=2):
+
+        """
+        Score interpretation:
+
+        scores near 1   imply a smoothly varying series
+        scores near 0   imply that there's no overall linear relationship between a data point and the following one
+                        (that is, plot(x[-length(x)],x[-1]) won't give a scatter plot with any apparent linearity)
+        scores near -1  suggest that the series is jagged in a particular way: if one point is above the mean, the next
+                        is likely to be below the mean by about the same amount, and vice versa."""
+
+        return np.corrcoef(np.array([firing_rates[0:len(firing_rates) - t], firing_rates[t:len(firing_rates)]]))
+
+    @staticmethod
+    def avg_corr_coeff(spike_train):
+
+        # Measure Average Pearson correlation coeffecient between neurons
+
+        corr_mat = np.corrcoef(np.asarray(spike_train).T)
+        avg_corr = np.sum(corr_mat, axis=1) / 200
+        corr_coeff = avg_corr.sum() / 200  # 2D to 1D and either upper  or lower half of correlation matrix.
+
+        return corr_mat, corr_coeff
+
+    @staticmethod
+    def spike_times(spike_train):
+
+        """ Get the time instants at which neuron spikes"""
+
+        times = np.where(spike_train == 1.)
+        return times
+
+    @staticmethod
+    def spike_time_intervals(spike_train):
+
+        """ Generate spike time intervals|spike_trains"""
+
+        spike_times = Statistics.spike_times(spike_train)
+        # isi = sorted(np.diff(spike_times)[-1])
+        isi = np.diff(spike_times)[-1]
+        return isi
+
+    @staticmethod
+    def hamming_distance(actual_spike_train, perturbed_spike_train):
+
+        """ Hamming distance between  """
+        hd = [np.count_nonzero(actual_spike_train[i] != perturbed_spike_train[i]) for i in range(len(actual_spike_train))]
+        return hd
+
+    # Fano Factor
+
+    @staticmethod
+    def fanofactor(spike_train,neuron,window_size):
+
+        """Investigate whether neuronal spike generation is a poisson process"""
+
+        # Choose activity of random neuron
+        neuron_act = spike_train[:, neuron]
+
+        # Divide total observations into 'tws' time windows of size 'ws' for a neuron 60
+
+        tws = np.split(neuron_act, window_size)
+        fr = []
+        for i in range(len(tws)):
+            fr.append(np.count_nonzero(tws[i]))
+
+        # print('Firing rate of the neuron during each time window of size %s is %s' %(ws,fr))
+
+        mean_firing_rate = np.mean(fr)
+        variance_firing_rate = np.var(fr)
+
+        fano_factor = variance_firing_rate / mean_firing_rate
+
+        return mean_firing_rate, variance_firing_rate, fano_factor
+
+    # Spike Source Entropy
+
+    @staticmethod
+    def spike_source_entropy(spike_train, neurons_in_reservoir):
+
+        # Uncertainty about the origin of spike from the network
+
+        # TODO: Remove neurons_in_reservoir in future versions
+
+        # Number of spikes from each neuron during the interval
+
+        n_spikes = np.count_nonzero(spike_train, axis=0)
+        p = n_spikes / np.count_nonzero(spike_train)  # Probability of each neuron that can generate spike in next step
+        # print(p)  # Note: pi shouldn't be zero
+        sse = np.sum([pi * np.log(pi) for pi in p]) / np.log(1 / neurons_in_reservoir)  # Spike source entropy
+
+        return sse
